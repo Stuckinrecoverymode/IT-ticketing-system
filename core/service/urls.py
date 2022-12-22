501cbba99125176893a8e65ec1e4c65949447885
Murat Y <93838693+Stuckinrecoverymode@users.noreply.github.com>
2022-12-23 01:56:55 +0300

    Add files via upload

---
+++ core/service/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('hakkimizda/', views.about, name='about'),
+    path('siparisal/', views.service_form, name='siparis'),
+    path('iletisim/', views.contact, name='contact'),
+    path('giris/', views.log_in, name='login'),
+]
